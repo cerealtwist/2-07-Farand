517a5763277f56b916bfb3c1854c61bbcb246e14
0Diy <ventriloquist225@gmail.com>
2022-06-07 16:39:46 +0700

    nm

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="">
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <div class="container">
-    <a class="navbar-brand" href="/">{{$nama}}</a>
+    <a class="navbar-brand" href="/">Farand</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
